ba0cf7553e795182e4e7780f16a47cc3b730c8fd
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-03-06 03:12:48 +0900

    アルバム画像と楽曲タイトルを表示

--- crypto-ai-hackathon/src/app/page.tsx
+++ crypto-ai-hackathon/src/app/page.tsx
@@ -1,23 +1,34 @@
 import styles from "./page.module.css";
 import SideBar from "@/component/SideBar/SideBar";
 import React from "react";
-import {fetchAlbumImageUrl} from "@/api/SpotifyApiHelper.server";
+import {fetchAlbumImageUrl} from "@/api/SpotifyApi.server";
+import Image from "next/image";
 
 const Page = () => {
     const imageUrl = fetchAlbumImageUrl();
+    const hasPlaylist = true;
     return (
         <main className={styles.main}>
-            <div className={styles.blurImage} style={{backgroundImage: `url('${imageUrl}')`}}/>
+            {hasPlaylist &&
+                <div className={styles.blurImage} style={{backgroundImage: `url('${imageUrl}')`}}/>
+            }
             <div className={styles.circleContainer}>
-                <PlaylistScreen/>
-                {/*<RecordScreen/>*/}
+                {hasPlaylist
+                    ? <PlaylistCircle imageUrl={imageUrl}/>
+                    : <RecordCircle/>}
             </div>
+            {hasPlaylist &&
+                <div className={styles.songTitle}>
+                    <div>Bling-Bang-Bang-Born</div>
+                    <div>Creepy Nuts</div>
+                </div>
+            }
             <SideBar/>
         </main>
     );
 }
 
-const RecordScreen = () => {
+const RecordCircle = () => {
     return <>
         <span className={styles.outerCircle} style={{scale: 2.3}}/>
         <span className={styles.outerCircle} style={{scale: 1.05}}/>
@@ -27,12 +38,16 @@ const RecordScreen = () => {
     </>;
 }
 
-const PlaylistScreen = () => {
+type PlaylistScreenProps = {
+    imageUrl: string,
+}
+
+const PlaylistCircle = ({imageUrl}: PlaylistScreenProps) => {
     return <>
         <span className={styles.outerCircle} style={{scale: 2.2}}/>
         <span className={styles.outerCircle} style={{scale: 1.3}}/>
-        <div className={`${styles.outerCircle} ${styles.micButton}`}>
-            <div className={styles.micButtonTitle}>環境音の分析を開始</div>
+        <div className={styles.outerCircle}>
+            <Image src={imageUrl} width={248} height={248} style={{borderRadius: "50%"}} alt={""}/>
         </div>
     </>;
 }
