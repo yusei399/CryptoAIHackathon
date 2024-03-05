8d9762e34ab2d6f7d0fc6618d71f78df606eee28
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-03-06 02:29:37 +0900

    ブラー背景画像の表示を実装

---
+++ crypto-ai-hackathon/src/api/SpotifyApiHelper.server.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export function fetchAlbumImageUrl() {
+    // 仮のURLを返す @TODO SpotifyのAPIから楽曲のアルバム画像URLを取得
+    return 'https://lh3.googleusercontent.com/ibUPY1ngpC1FDciRsyS89-dDrYyYV-4fVx6Ou9DSnbNJ1Qp45kay4Bqn1_2lPqORc4dOHDvUer9_QQfZ6g=w544-h544-l90-rj';
+}
--- crypto-ai-hackathon/src/app/page.tsx
+++ crypto-ai-hackathon/src/app/page.tsx
@@ -1,17 +1,40 @@
 import styles from "./page.module.css";
 import SideBar from "@/component/SideBar/SideBar";
+import React from "react";
+import {fetchAlbumImageUrl} from "@/api/SpotifyApiHelper.server";
 
-export default function Home() {
+const Page = () => {
+    const imageUrl = fetchAlbumImageUrl();
     return (
         <main className={styles.main}>
+            <div className={styles.blurImage} style={{backgroundImage: `url('${imageUrl}')`}}/>
             <div className={styles.circleContainer}>
-                <span className={styles.outerCircle} style={{scale: 2.3}}/>
-                <span className={styles.outerCircle} style={{scale: 1.05}}/>
-                <div className={`${styles.outerCircle} ${styles.micButton}`}>
-                    <div className={styles.micButtonTitle}>環境音の分析を開始</div>
-                </div>
+                <PlaylistScreen/>
+                {/*<RecordScreen/>*/}
             </div>
             <SideBar/>
         </main>
     );
 }
+
+const RecordScreen = () => {
+    return <>
+        <span className={styles.outerCircle} style={{scale: 2.3}}/>
+        <span className={styles.outerCircle} style={{scale: 1.05}}/>
+        <div className={`${styles.outerCircle} ${styles.micButton}`}>
+            <div className={styles.micButtonTitle}>環境音の分析を開始</div>
+        </div>
+    </>;
+}
+
+const PlaylistScreen = () => {
+    return <>
+        <span className={styles.outerCircle} style={{scale: 2.2}}/>
+        <span className={styles.outerCircle} style={{scale: 1.3}}/>
+        <div className={`${styles.outerCircle} ${styles.micButton}`}>
+            <div className={styles.micButtonTitle}>環境音の分析を開始</div>
+        </div>
+    </>;
+}
+
+export default Page
