54299a20f962e91b6d071fcab56e4607a8f9faf0
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-03-07 23:12:24 +0900

    型チェックを修正

--- crypto-ai-hackathon/src/app/page.tsx
+++ crypto-ai-hackathon/src/app/page.tsx
@@ -22,7 +22,7 @@ const Page = () => {
     );
 }
 
-const getCookie = (name) => {
+const getCookie = (name: string) => {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
